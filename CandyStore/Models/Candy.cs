26dbdf488fc510337370a60e93e8e47978834321
Olivia Walter <olivia.walter@edu.edugrade.se>
2022-05-23 13:29:00 +0200

    init Salefunctions

--- CandyStore/Models/Candy.cs
+++ CandyStore/Models/Candy.cs
@@ -1,6 +1,8 @@
-﻿namespace CandyStore.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-public class Candy
+namespace CandyStore.Models;
+
+public partial class Candy
 {
     public int CandyID { get; set; }
     public string Name { get; set; } = default!;
@@ -8,8 +10,18 @@ public class Candy
     public decimal? Price { get; set; }
     public string? ImageURL { get; set; }
     public string? ImageThumbnailURL { get; set; }
-    public bool IsOnSale { get; set; }
     public bool IsInStock { get; set; }
     public int CategoryID { get; set; }
     public Category? Category { get; set; }
+
+    
+}
+public partial class Candy
+{
+    //Sale 
+    public DateTime SaleStart { get; set; }
+    public DateTime SaleEnd { get; set; }
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal SalePrice { get; set; }
+    public bool IsOnSale { get; set; }
 }
--- CandyStore/ViewModels/CandyListViewModel.cs
+++ CandyStore/ViewModels/CandyListViewModel.cs
@@ -5,5 +5,6 @@ namespace CandyStore.ViewModels;
 public class CandyListViewModel
 {
     public IEnumerable<Candy> Candy { get; set; } = default!;
+    public Candy? SaleCandy { get; set; }
     public string? Category { get; set; }
 }
